2de48a8f27e119534e7570b091c234e8a4ae3089
MathSena <MathSena07@hotmail.com>
2025-04-24 22:08:09 -0300

    first commit

---
+++ aulas/01-fundamentos-react.js/src/App.jsx
@@ -0,0 +1,23 @@
+//JSX = Javascript + XML
+
+import Post from "./Post"
+
+function App() {
+  
+  return (
+    <>
+    <h1>Hello World!</h1>
+    <div><Post author="Matheus Sena"
+    content = "Esse é um post de exemplo" 
+    />
+    <Post author="Memphis Depay"
+    content = "Vai Corinthians" 
+    />
+    <Post />
+    <Post />
+    </div>
+    </>
+  )
+}
+
+export default App
